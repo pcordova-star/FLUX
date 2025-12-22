14ec75450f1b61c767bb5b705bff1a8cbd898d6d
Paulo Córdova <pcordova@woken.cl>
2025-12-22 23:03:01 +0000

    reemplaza todos los datos por estos para que funcione // Import the func

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -1,17 +1,21 @@
 'use client';
 
+// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBs21YoH_3Fi-u3Bt8MJClq09gEdQLBW9o",
+  authDomain: "studio-7575474202-582d8.firebaseapp.com",
+  projectId: "studio-7575474202-582d8",
+  storageBucket: "studio-7575474202-582d8.firebasestorage.app",
+  messagingSenderId: "1057800767082",
+  appId: "1:1057800767082:web:4ded3e72275631c6516c13"
 };
 
 // Initialize Firebase
